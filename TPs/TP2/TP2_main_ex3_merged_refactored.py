3ceeba5b0e9ec17a2bd418928b2712335270355f
TZ-Boomer <julienmarty0819@gmail.com>
2024-12-03 15:31:09 +0100

    TP2 report

--- TPs/TP2/TP2_main_ex3_merged_refactored.py
+++ TPs/TP2/TP2_main_ex3_merged_refactored.py
@@ -23,11 +23,11 @@ FILTER_EDGES_THRESHOLD = 0.3
 # Size of the kernel (cube) that avoid multiple similar circles (same center and radius), depend on the scale of the image
 LOCAL_MAX_KERNEL_SIZE = 35
 
-IMAGE_REDUCTION_LEVELS = 1 # Image divided by SCALE_FACTOR at each iteration
+IMAGE_REDUCTION_LEVELS = 3 # Image divided by SCALE_FACTOR at each iteration
 SCALE_FACTOR = 2
 MIN_DETECT_LEVEL = 0.5
 CIRCLE_CENTER_UPDATE_RANGE = 1
-CIRCLE_RADIUS_UPDATE_RANGE = 8
+CIRCLE_RADIUS_UPDATE_RANGE = 11
 
 
 def main():
@@ -50,7 +50,7 @@ def image_reduction(original_image):
     sobel_data = sobel_filter(original_image, threshold_ratio=FILTER_EDGES_THRESHOLD)
     edges_images = [sobel_data['edges_image']]
 
-    local_maxima_coords = []
+    local_maxima_accumulator = []
 
     # Create reduced images
     for i in range(IMAGE_REDUCTION_LEVELS):
@@ -64,8 +64,8 @@ def image_reduction(original_image):
         print(f"===== Iteration number : {i}")
 
         # Scale up the coordinates and radius
-        if local_maxima_coords:
-            local_maxima_coords = scale_coordinates(local_maxima_coords)
+        if local_maxima_accumulator:
+            local_maxima_accumulator = scale_coordinates(local_maxima_accumulator)
 
         # Apply Sobel filter to get gradient direction and other data
         sobel_data = sobel_filter(original_images[i], threshold_ratio=FILTER_EDGES_THRESHOLD)
@@ -79,9 +79,9 @@ def image_reduction(original_image):
                 image_gradient_direction=gradient_direction,
                 current_iteration = i,
                 max_radius=-1,
-                local_maxima_coords=None
+                local_maxima_accumulator=None
             )
-            local_maxima_coords.extend(initial_circles)
+            local_maxima_accumulator.extend(initial_circles)
         else:
             # Detect small circles
             new_circles = hough_method(
@@ -97,23 +97,23 @@ def image_reduction(original_image):
                 image_gradient_direction=gradient_direction,
                 current_iteration=i,
                 max_radius=-1,
-                local_maxima_coords=local_maxima_coords
+                local_maxima_accumulator=local_maxima_accumulator
             )
-            local_maxima_coords = updated_circles
+            local_maxima_accumulator = updated_circles
 
             # Keep circles with high probability
             best_new_circles = [circle for circle in new_circles if circle[3] > MIN_DETECT_LEVEL]
-            local_maxima_coords.extend(best_new_circles)
+            local_maxima_accumulator.extend(best_new_circles)
 
-        print("Coordinates :", local_maxima_coords)
-        draw_circles(original_images[i], local_maxima_coords)
+        print("Coordinates :", local_maxima_accumulator)
+        draw_circles(original_images[i], local_maxima_accumulator)
         display_images(
             [cv2.cvtColor(original_images[i], cv2.COLOR_BGR2RGB)],
             titles=[f'Detected Circles at Scale {i}'],
             figsize=(8, 8)
         )
 
-    draw_circles(final_image, local_maxima_coords)
+    draw_circles(final_image, local_maxima_accumulator)
     display_images(
         [cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB)],
         titles=['Final Detected Circles'],
@@ -121,15 +121,16 @@ def image_reduction(original_image):
     )
 
 
-def hough_method(edges_image, image_gradient_direction, current_iteration, max_radius=-1, local_maxima_coords=None):
-    if local_maxima_coords is None:
+def hough_method(edges_image, image_gradient_direction, current_iteration, max_radius=-1, local_maxima_accumulator=None):
+    if local_maxima_accumulator is None:
         accumulator = populate_accumulator(edges_image, image_gradient_direction, max_radius)
+        sorted_local_maxima_accumulator = get_local_maxima(accumulator, current_iteration)
     else:
-        accumulator = update_accumulator(edges_image, local_maxima_coords)
+        sorted_local_maxima_accumulator = update_accumulator(edges_image, local_maxima_accumulator)
 
-    sorted_local_maxima_coords = get_local_maxima(accumulator, current_iteration)
+    print("HOUGH LEN : ", len(sorted_local_maxima_accumulator))
 
-    return sorted_local_maxima_coords
+    return sorted_local_maxima_accumulator
 
 
 def populate_accumulator(edges_image, image_gradient_direction, max_radius):
@@ -165,10 +166,12 @@ def populate_accumulator(edges_image, image_gradient_direction, max_radius):
     return accumulator
 
 
-def update_accumulator(edges_image, local_maxima_coords):
+def update_accumulator(edges_image, local_maxima_accumulator):
     print("_____________ Updating accumulator...")
     # Get the dimensions of the image
     height, width = edges_image.shape[:2]
+
+    new_local_maxima_accumulator = []
 
     r_max = height - 1
     c_max = width - 1
@@ -187,8 +190,7 @@ def update_accumulator(edges_image, local_maxima_coords):
     accumulator = np.zeros((n_col, n_row, n_rad))
 
     edge_coordinates = get_edges_coordinates(edges_image)
-
-    for circle in local_maxima_coords:
+    for circle in local_maxima_accumulator:
         cir_x, cir_y, cir_rad, _ = circle
 
         # +1 because the upper limit is excluded from the interval
@@ -230,7 +232,19 @@ def update_accumulator(edges_image, local_maxima_coords):
                                 raise Exception(f"Unexpected index value for the accumulator "
                                                 f"acc_c_idx : {acc_c_idx}, acc_r_idx : {acc_r_idx}, acc_rad_idx : {acc_rad_idx}")
 
-    return accumulator
+        max_value = np.max(accumulator)
+        max_indices = np.unravel_index(np.argmax(accumulator), accumulator.shape)
+
+        # Combine coordinates and values
+        row = (max_indices[0], max_indices[1], max_indices[2], max_value)
+        # Add the updated circle to the final list
+        new_local_maxima_accumulator.append(row)
+        # Reinitialize accumulator for the next circle
+        accumulator = np.zeros((n_col, n_row, n_rad))
+
+    print("LEN new_local_maxima_accumulator : ", len(new_local_maxima_accumulator))
+
+    return new_local_maxima_accumulator
 
 
 def display_sobel_filter_results(original_image, edges_image, sobel_data):
@@ -254,41 +268,41 @@ def display_sobel_filter_results(original_image, edges_image, sobel_data):
 
     display_images(images_to_show, titles=titles, figsize=(15, 5))
 
-def scale_coordinates(local_maxima_coords):
+def scale_coordinates(local_maxima_accumulator):
     # Convert accumulator indices to circle parameters
-    local_maxima_coords = [
+    local_maxima_accumulator = [
         [
             (coord[0] * DELTA_C) + MIN_C,  # x-coordinate
             (coord[1] * DELTA_R) + MIN_R,  # y-coordinate
             (coord[2] * DELTA_RAD) + MIN_RAD,  # radius
             coord[3]  # Likeliness of the circle
         ]
-        for coord in local_maxima_coords
+        for coord in local_maxima_accumulator
     ]
 
     # Scale up the circle parameters
-    local_maxima_coords = [
+    local_maxima_accumulator = [
         [
             coord[0] * SCALE_FACTOR,  # Scaled x-coordinate
             coord[1] * SCALE_FACTOR,  # Scaled y-coordinate
             coord[2] * SCALE_FACTOR,  # Scaled radius
             coord[3]
         ]
-        for coord in local_maxima_coords
+        for coord in local_maxima_accumulator
     ]
 
     # Map back to accumulator indices for the new scale
-    local_maxima_coords = [
+    local_maxima_accumulator = [
         [
             int((coord[0] - MIN_C) / DELTA_C),  # x_idx
             int((coord[1] - MIN_R) / DELTA_R),  # y_idx
             int((coord[2] - MIN_RAD) / DELTA_RAD),  # radius_idx
             coord[3]
         ]
-        for coord in local_maxima_coords
+        for coord in local_maxima_accumulator
     ]
 
-    return local_maxima_coords
+    return local_maxima_accumulator
 
 
 def get_local_maxima(accumulator, current_iteration):
@@ -308,14 +322,63 @@ def get_local_maxima(accumulator, current_iteration):
     local_max_values = accumulator[maxima]
 
     # Combine coordinates and values
-    local_maxima_coords = [
+    local_maxima_accumulator = [
         (coord[0], coord[1], coord[2], val)
         for coord, val in zip(local_max_coords, local_max_values)
     ]
 
     # Sort and select top N circles
-    sorted_local_maxima_coords = sorted(local_maxima_coords, key=lambda x: x[3], reverse=True)
-    return sorted_local_maxima_coords[:N_CIRCLES]
+    sorted_local_maxima_accumulator = sorted(local_maxima_accumulator, key=lambda x: x[3], reverse=True)
+
+    return sorted_local_maxima_accumulator[:N_CIRCLES]
+
+# Way to slow
+def get_local_maxima_v0(accumulator):
+    n_col, n_row, n_rad = accumulator.shape
+
+    padding = int(LOCAL_MAX_KERNEL_SIZE / 2)
+    kernel = list(range(-padding, padding + 1))
+
+    # Initialize an empty list to store the coordinates of local maxima
+    local_maxima_accumulator = []
+
+    # Loop through each element in the 3D array, avoiding the borders
+    for x in range(padding, n_col - padding):
+        for y in range(padding, n_row - padding):
+            for z in range(padding, n_rad - padding):
+                # Get the current value
+                current_value = accumulator[x, y, z]
+
+                # Flag to check if this is a local maximum
+                is_local_maximum = True
+
+                # Loop through the neighboring cells
+                for dx in kernel:
+                    for dy in kernel:
+                        for dz in kernel:
+                            if dx == 0 and dy == 0 and dz == 0:
+                                continue  # Skip the current cell itself
+
+                            # Compare with neighboring cell
+                            neighbor_value = accumulator[x + dx, y + dy, z + dz]
+                            if neighbor_value >= current_value:
+                                is_local_maximum = False
+                                break  # Stop checking neighbors if one is greater or equal
+                        if not is_local_maximum:
+                            break
+                    if not is_local_maximum:
+                        break
+
+                # If it's a local maximum, save its coordinates
+                if is_local_maximum:
+                    acc_value = accumulator[x, y, z]
+                    local_maxima_accumulator.append((x, y, z, acc_value))
+
+    # Sort the local_maxima_accumulator in descending order based on the accumulator value
+    sorted_local_maxima_accumulator = sorted(local_maxima_accumulator, key=lambda x: x[3], reverse=True)
+    sorted_local_maxima_accumulator = sorted_local_maxima_accumulator[:N_CIRCLES]
+
+    return sorted_local_maxima_accumulator
 
 
 def resize_image(image):
@@ -368,7 +431,7 @@ def draw_circles(image, circles):
         image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
     color = (0, 255, 0)  # Green color in BGR
-    thickness = 2  # Increased thickness for better visibility
+    thickness = 2
 
     for x_idx, y_idx, radius_idx, _ in circles:
         x = int((x_idx * DELTA_C) + MIN_C)
